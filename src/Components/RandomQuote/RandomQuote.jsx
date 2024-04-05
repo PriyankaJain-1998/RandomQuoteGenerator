4156dffbe2f4d861386ed6bf5022b7b92f9b7b9b
PriyankaJain-1998 <jainpriyanka2598@gmail.com>
2024-04-05 17:39:35 -0400

    First Commit

---
+++ src/Components/RandomQuote/RandomQuote.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react'
+import './RandomQuote.css'
+import twitter_icon from '../Assets/Twitter.png'
+import reload_icon from '../Assets/reload.png'
+
+export const RandomQuote = () => {
+
+    let quotes = [];
+
+    async function loadQuote(){
+        const response = await fetch("https://type.fit/api/quotes")
+        quotes = await response.json();
+    }
+
+    const [quote, setQuote] = useState({
+        text: "Difficulties increase the nearer we to our goal", 
+        author: "Johann Wolfgang Von Goethe",
+    });
+
+    const random = () =>{
+        const select = quotes[Math.floor(Math.random()*quotes.length)]
+        setQuote (select);
+    }
+
+    const twitter = () =>{
+        window.open("https://twitter.com/intent/tweet?text=${quote.text} - ${quote.author.split(',')[0]}");
+    }
+    
+    loadQuote();
+
+  return (
+    <div className='container'>
+        <div className='quote'>{quote.text}</div>
+        <div>
+            <div className="line"></div>
+            <div className="bottom">
+                <div className="author">- {quote.author.split(',')[0]}</div>
+                <div className="icons">
+                    <img src={reload_icon} onClick={()=>{random()}} alt="" />
+                    <img src={twitter_icon} onClick={()=>{twitter()}} alt="" />
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+export default RandomQuote
